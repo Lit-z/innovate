a2d904f768038042b0321374dc8206f630670981
Jamie <gee.jamie.a@gmail.com>
2022-05-05 10:22:22 +0100

    fixed act1

--- python_stuff/welcome_act1.py
+++ python_stuff/welcome_act1.py
@@ -3,9 +3,11 @@ welcome = 'Welcome to Code Nation!'
 str_len = len(welcome)
 
 # checking for even by checking for /2 with no remainders (%2 == 0)
-if str_len % 2 == 0:
-    print(welcome)
-    print('Things looking pretty even')
-else:
-    print(welcome)
-    print("We're odd around here")
+def even_check():
+    if str_len % 2 == 0:  
+        print(f'the string "{welcome}" is an amazing {str_len} characters long it\'s looking pretty even')
+    else:
+        print(f'the string "{welcome}" is an amazing {str_len} characters long and We\'re odd around here')
+
+# calling the function
+even_check()
